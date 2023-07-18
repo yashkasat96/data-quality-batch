506a013a9f19f65fdb3def0e98fe732dba8a6f3c
daidipya <daidipya.thakare@clairvoyantsoft.com>
2023-07-18 11:29:33 +0530

    Merging data comparator to data quality

--- src/utils.py
+++ src/utils.py
@@ -1,20 +1,30 @@
+import json
+from google.cloud.storage import Client, blob
 from pyspark.sql import SparkSession
 
 
-def getSparkSession():
+def get_spark_session():
     return SparkSession.builder \
         .appName("Data Quality").enableHiveSupport() \
         .getOrCreate()
 
 
+def get_empty_data_frame(schema):
+    return get_spark_session().createDataFrame(get_spark_session().sparkContext.emptyRDD(),
+                                             schema=schema)
+def get_unique_id():
+    while True:
+        number = str(BLANK.join(random.choices(string.digits, k=8)))
+        if not number.startswith('0'):
+            return number
 
 def read_file(file_path):
     keys = {}
-    is_json_file = file_path.endswith(JSON_EXTENSION)
-    is_properties_file = file_path.endswith(PROPERTIES_EXTENSION)
+    is_json_file = file_path.endswith('.json')
+    is_properties_file = file_path.endswith('.properties')
     global data
 
-    if file_path.startswith(GOOGLE_STORAGE_PATH):
+    if file_path.startswith('gs://'):
         data = gs_reader(file_path)
 
         if is_json_file:
@@ -23,8 +33,8 @@ def read_file(file_path):
         if is_properties_file:
 
             for line in data.splitlines():
-                if EQUAL_TO in line:
-                    name, value = line.split(EQUAL_TO, 1)
+                if '=' in line:
+                    name, value = line.split('=', 1)
                     keys[name.strip()] = value.strip()
             data = keys
     else:
@@ -35,8 +45,8 @@ def read_file(file_path):
         if is_properties_file:
             with open(file_path) as f:
                 for line in f:
-                    if EQUAL_TO in line:
-                        name, value = line.split(EQUAL_TO, 1)
+                    if '=' in line:
+                        name, value = line.split('=', 1)
                         keys[name.strip()] = value.strip()
                 data = keys
 
@@ -45,7 +55,7 @@ def read_file(file_path):
 
 def gs_reader(path):
     length_of_google_storage_prefix = 5
-    bucket_name = path[length_of_google_storage_prefix:].split(FORWARD_SLASH)[ZERO]
+    bucket_name = path[length_of_google_storage_prefix:].split('/')[0]
     file_name = path[6 + len(bucket_name):]
     bucket = Client().get_bucket(bucket_name)
     return bucket.get_blob(file_name).download_as_text(encoding="utf-8")
